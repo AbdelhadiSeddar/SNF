12764e97530577c8d0d5410095548fa669c896c4
Abdelhadi Seddar <abdelhadiseddar9114@gmail.com>
2023-12-27 12:23:37 +0100

    Added SQLite 3
    This project will use SQLite to save Temporarly Client Informations , Console Logs, this is used for cases of crashes, so it will be able to roll back and be able to diagnose at a faster rate

--- Server/C/SRBNP/_Imports.h
+++ Server/C/SRBNP/_Imports.h
@@ -14,3 +14,8 @@
 #include "network.h"
 #include "thpool.h"
 #pragma endregion
+
+#pragma region [Third-party libraries]
+///     SQLite Will only be used for Saving Temporary Clients info / Logs
+#include "../SQLite/sqlite3.h"
+#pragma endregion
