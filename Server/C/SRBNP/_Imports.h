3f0bea32166cc26a42e931cda32dab45b5f404b6
Seddar Abdelhadi <abdelhadiseddar9114@gmail.com>
2024-01-28 15:45:59 +0100

    Starting removal Of SQLite
    Changes to align with the deletion of sqlite
    though the code is not compileable for now

--- Server/C/SRBNP/_Imports.h
+++ Server/C/SRBNP/_Imports.h
@@ -26,12 +26,6 @@
 #pragma endregion
 
 #pragma region [Non-Standard]
-#include "utility.h"
-#include "network.h"
-#include "thpool.h"
-#include "epoll.h"
-#include "sqlite.h"
-#include "clt.h"
 #include "_BASE_OPCODE.h"
 #include "clt.h"
 #include "epoll.h"
@@ -42,8 +36,4 @@
 #include "utility.h"
 #pragma endregion
 
-#pragma region [Third-party libraries]
-///     SQLite Will only be used for Saving Temporary Clients info / Logs
-#include "../SQLite/sqlite3.h"
-#pragma endregion
 
