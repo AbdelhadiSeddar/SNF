449dd9a7147ddd3cd8a11942d82c07b23d996250
Abdelhadi Seddar <abdelhadiseddar9114@gmail.com>
2024-02-08 22:26:36 +0100

    Server/C Change in makefile, and added compilation check
    in src/SRBNP.h: Added a check for the availability of atomics.
    
    in makefile:
    - changed compiler of object files from gcc to user-defined variable $(COMPILER)
    
    - Added make install which puts this library into standard library locations ( didnt find a way to get default includes using shell commands, way have the risk of not being unix cross-compatible )
    - split clean rule to
       clean: removes all generated folders/files using make
      prepare: creates required folders
    - instaid of prebuild, that rule is named headers as it prepares header files

--- Server/C/src/SRBNP.h
+++ Server/C/src/SRBNP.h
@@ -3,22 +3,30 @@
 
 #define _GNU_SOURCE
 
-#pragma region [Standard]
+#pragma region[Standard]
 #include <errno.h>
 #include <fcntl.h>
-#include <netdb.h> 
+#include <netdb.h>
 #include <unistd.h>
 #include <semaphore.h>
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <netinet/in.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
 #include <sys/epoll.h>
-#include <sys/socket.h> 
-#include <sys/types.h> 
+#include <sys/socket.h>
+#include <sys/types.h>
 #pragma endregion
 
-#pragma region [Non-Standard]
+#if __STDC_VERSION__ < 201112L 
+#error Use C 11 or later !
+#else
+#if __STDC_NO_ATOMICS__ == 1
+#error Atomics not Supported, Abort.
+#endif
+#endif
+
+#pragma region[Non-Standard]
 #include <uuid/uuid.h>
 #include <pthread.h>
 #include <math.h>
